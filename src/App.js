d9cfa89adfa88e41dd8a6dc4727f74bf905be257
gmirg <gofthet@gmail.com>
2022-11-30 18:04:35 +0100

    pantalla de carga ok y colores encaminado

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [showScore, setShowScore] = useState(false);
-  const [color,setColor] = useState('white')
+  const [className, setClassName] = useState('answer-button')
   const Questions = () => {
     fetch('https://opentdb.com/api.php?amount=50&category=32&type=multiple')
       .then(res => res.json())
@@ -20,39 +20,40 @@ function App() {
           incorrect.push(element.incorrect_answers);
           correct.push(element.correct_answer);
         });
-        incorrect.forEach((i) => {
+        incorrect.forEach((element, i) => {
           incorrect[i].splice(Math.floor(Math.random() * (incorrect.length + 1)), 0, correct[i])
         }
         )
         setAnswers(incorrect)
         setData(random);
-        console.log(data)
-        console.log(answers)
+        // console.log(data)
+        // console.log(answers)
       });
   }
   useEffect(() => {
     Questions()
   }, [])
-  useEffect(() => {
-    console.log(data)
-  }, [data])
+  // useEffect(() => {
+  //   console.log(data)
+  // }, [data])
   const delay = (duration, callback) => {
     setTimeout(() => {
       callback();
     }, duration);
   };
-  const checkAnswer = (value) => {
+  const checkAnswer = (value, e) => {
     if (value === data[currentQuestion].correct_answer) {
       setScore(score + 1);
-      setColor('#2f922f3c');
+      console.log(e);
+      e.setClassName("answer-button correct")
     } else {
-      setColor('#ff33333c');
+      console.log(e);
+      // e.setClassName("answer-button incorrect");
     }
     const nextQuestion = currentQuestion + 1;
     delay(500, () => {
       if (nextQuestion < data.length) {
         setCurrentQuestion(nextQuestion);
-        setColor('white');
       } else {
         setShowScore(true);
       }
@@ -63,14 +64,17 @@ function App() {
     setScore(0);
     setCurrentQuestion(0);
     setShowScore(false);
-    setColor('white');
+    setClassName("answer-button");
   };
   return (
     <div className="App">
-      {showScore ? (
+      {data.length == 0 ? <div className='score-section'>
+        <img src='qmark.gif'></img>
+        </div> :
+      showScore ? (
         <div className='score-section'>
           You scored {score} out of {data.length}
-          <button className='button'onClick={() => restartGame()}>START AGAIN</button>
+          <button className='button' onClick={() => restartGame()}>START AGAIN</button>
         </div>
       ) : (
         <>
@@ -79,32 +83,28 @@ function App() {
             <div className='question-count'>
               <span>Question {currentQuestion + 1}</span>
             </div>
-            <div className='question-text' dangerouslySetInnerHTML={{__html: data && data.length > 0 && data[currentQuestion].question}}></div>
+            <div className='question-text' dangerouslySetInnerHTML={{ __html: data && data.length > 0 && data[currentQuestion].question }}></div>
           </div>
           <div className='answer-section'>
             <input
               className="answer-button"
               type="button"
-              style={{background: color}}
-              onClick={(e) => checkAnswer(e.target.value)}
+              onClick={(e) => checkAnswer(e.target.value, e)}
               value={answers && answers.length > 0 && answers[currentQuestion][0]} />
             <input
               className="answer-button"
               type="button"
-              style={{background: color}}
-              onClick={(e) => checkAnswer(e.target.value)}
+              onClick={(e) => checkAnswer(e.target.value, e)}
               value={answers && answers.length > 0 && answers[currentQuestion][1]} />
             <input
               className="answer-button"
               type="button"
-              style={{background: color}}
-              onClick={(e) => checkAnswer(e.target.value)}
+              onClick={(e) => checkAnswer(e.target.value, e)}
               value={answers && answers.length > 0 && answers[currentQuestion][2]} />
             <input
               className="answer-button"
               type="button"
-              style={{background: color}}
-              onClick={(e) => checkAnswer(e.target.value)}
+              onClick={(e) => checkAnswer(e.target.value, e)}
               value={answers && answers.length > 0 && answers[currentQuestion][3]} />
           </div>
         </>
